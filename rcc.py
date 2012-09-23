2da4fb68bc03f23268da4b0a3d544312a31d9174
Onuralp SEZER <thunderbirdtr@gmail.com>
2012-09-23 18:27:15 +0300

    Enviroment and sub menus added Comments and text fixed

--- rcc.py
+++ rcc.py
@@ -25,7 +25,7 @@ statsmenu = None
 softwaremenu = None
 
 
-
+# The Raspberry-PI Control Center (RCC) Top Menu #
 def topmenu():
 
   screen.keypad(1)
@@ -82,7 +82,7 @@ def topmenu():
     elif x != ord('\n'):
       curses.flash()
   return ord(str(pos))
-
+# The Raspberry-PI Control Center (RCC)/Basic Setup #
 def submenu1():
   screen.keypad(1)
   curses.init_pair(1,curses.COLOR_BLUE, curses.COLOR_WHITE)
@@ -165,7 +165,7 @@ def submenu1():
     elif x != ord('\n'):
       curses.flash()
   return ord(str(pos))
- 
+# The Raspberry-PI Control Center (RCC)/ Advanced Tune and Setup #
 def submenu2():
   screen.keypad(1)
   curses.init_pair(1,curses.COLOR_BLUE, curses.COLOR_WHITE)
@@ -191,7 +191,7 @@ def submenu2():
     if pos==3:
       screen.addstr(7,4, "3 - Install Zram (RAM content compression - expanding to 512 MB)", h)
     else:
-      screen.addstr(7,4, "3 - Install Zram (RAM content compression - expanding to 512 MB)"", n)
+      screen.addstr(7,4, "3 - Install Zram (RAM content compression - expanding to 512 MB)", n)
     if pos==4:
       screen.addstr(8,4, "4 - Expand Swap Size (512MB)", h)
     else:
@@ -236,7 +236,7 @@ def submenu2():
     elif x != ord('\n'):
       curses.flash()
   return ord(str(pos))
-
+# The Raspberry-PI Control Center (RCC)/ Board and Sys Information #
 def submenu3():
   screen.keypad(1)
   curses.init_pair(1,curses.COLOR_BLUE, curses.COLOR_WHITE)
@@ -264,9 +264,9 @@ def submenu3():
     else:
       screen.addstr(7,4, "3 - Chipset and Proc Info", n)
     if pos==4:
-      screen.addstr(8,4, "4 - Current speed with Turbo mode active", h)
+      screen.addstr(8,4, "4 - Current CPU Speed", h)
     else:
-      screen.addstr(8,4, "4 - Current speed with Turbo mode active", n)
+      screen.addstr(8,4, "4 - Current CPU Speed", n)
     if pos==5:
       screen.addstr(9,4, "5 - Installed RAM / Free RAM", h)
     else:
@@ -276,13 +276,13 @@ def submenu3():
     else:
       screen.addstr(10,4, "6 - GPIO PIN info ONLINE/OFFLINE", n)
     if pos==7:
-      screen.addstr(11,4, "7 - Network Status (ETH, WIFI, Broadband)", h)
+      screen.addstr(11,4, "7 - Network Status (ETH, WIFI)", h)
     else:
-      screen.addstr(11,4, "7 - Network Status (ETH, WIFI, Broadband)", n)
+      screen.addstr(11,4, "7 - Network Status (ETH, WIFI)", n)
     if pos==8:
-      screen.addstr(12,4, "8 - USB - grep lsusb filtered info", h)
+      screen.addstr(12,4, "8 - USB info", h)
     else:
-      screen.addstr(12,4, "8 - USB - grep lsusb filtered info", n)
+      screen.addstr(12,4, "8 - USB info", n)
     if pos==9:
       screen.addstr(13,4, "9 - Return to Top Menu", h)
     else:
@@ -325,18 +325,120 @@ def submenu3():
     elif x != ord('\n'):
       curses.flash()
   return ord(str(pos))
+# Basic Setup /Environment Menu #
+def environmentmenu():
+	
+  screen.keypad(1)
+  curses.init_pair(1,curses.COLOR_BLUE, curses.COLOR_WHITE)
 
+  pos=1 
+  x = None 
+  h = curses.color_pair(1) #h is the coloring for a highlighted menu option
+  n = curses.A_NORMAL #n is the coloring for a non highlighted menu option
+  
+  while x !=ord('\n'):
+    screen.clear() 
+    screen.border(0)
+    screen.addstr(0,18, "The Raspberry-PI Control Center (RCC)", curses.A_STANDOUT) # Title
+    screen.addstr(4,2, "Basic Setup /Environment Menu", curses.A_STANDOUT)
+	
+    if pos==1:
+      screen.addstr(5,4, "1 - Time Sync", h)
+    else:
+      screen.addstr(5,4, "1 - Time Sync", n)
+    if pos==2:
+      screen.addstr(6,4, "2 - Language Setting", h)
+    else:
+      screen.addstr(6,4, "2 - Language Setting", n)
+    if pos==3:
+      screen.addstr(7,4, "3 - Locale Setup", h)
+    else:
+      screen.addstr(7,4, "3 - Locale Setup", n)
+    if pos==4:
+      screen.addstr(8,4, "4 - Console Setup", h)
+    else:
+      screen.addstr(8,4, "4 - Console Setup", n)
+    if pos==5:
+      screen.addstr(9,4, "5 - Check Updates", h)
+    else:
+      screen.addstr(9,4, "5 - Check Updates", n)
+    if pos==6:
+      screen.addstr(10,4, "6 - Check Updates", h)
+    else:
+      screen.addstr(10,4, "6 - Check Updates", n)
+    if pos==7:
+      screen.addstr(11,4, "7 - Add User and Group", h)
+    else:
+      screen.addstr(11,4, "7 - Add User and Group", n)
+    if pos==8:
+      screen.addstr(11,4, "8 - Return to the Basic Setup Menu", h)
+    else:
+      screen.addstr(11,4, "8 - Return to the Basic Setup Menu", n)      
+    screen.refresh()
+    x = screen.getch() # Gets user input
+
+    if x == ord('1'):
+      pos = 1
+    elif x == ord('2'):
+      pos = 2
+    elif x == ord('3'):
+      pos = 3
+    elif x == ord('4'):
+      pos = 4
+    elif x == ord('5'):
+      pos = 5
+    elif x == ord('6'):
+      pos = 6
+    elif x == ord('7'):
+      pos = 7
+    elif x == ord('8'):
+      pos = 8
+    elif x == 258:
+      if pos < 8:
+
+	pos += 1
+      else: pos = 1
+    elif x == 259:
+      if pos > 1:
+	  pos += -1
+	  
+      else: pos = 8
+    elif x != ord('\n'):
+      curses.flash()
+  return ord(str(pos))
+	
+# All top and sub menu command part #	
 while getin != ord('4'):
   sub1get = None 
   sub2get = None
   sub3get = None
+  env1get = None
   getin = topmenu() 
   
   if getin == ord('1'): # Topmenu option 1,
     while sub1get !=ord('9'): 
       sub1get = submenu1() # ------Submenu 1 Commands------#
       if sub1get == ord('1'): #Submenu 1 option 1
-	() 
+	while env1get !=ord('8'):
+	  env1get = environmentmenu()
+      if env1get == ord('1'): # Envmenu 1 option 1
+	()
+      elif env1get == ord('2'): # Envmenu 1 option 2
+	()
+      elif env1get == ord('3'): # Envmenu 1 option 3
+	()
+      elif env1get == ord('4'): # Envmenu 1 option 4
+	()
+      elif env1get == ord('5'): # Envmenu 1 option 5
+	()
+      elif env1get == ord('6'): # Envmenu 1 option 6
+	()
+      elif env1get == ord('7'): # Envmenu 1 option 7
+	()
+      elif env1get == ord('8'): # Envmenu 1 option 8
+	os.system('')	
+
+	
       elif sub1get == ord('2'): # Submenu 1 option 2
 	()
       elif sub1get == ord('3'): # Submenu 1 option 3
